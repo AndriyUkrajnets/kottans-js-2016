d58245783611bc396b325337a5f77401ff4c7719
AndriyUkrajnets <aukrajnets@gmail.com>
2016-06-13 17:22:39 +0200

    deepAssign function

--- week03/deepAssign.js
+++ week03/deepAssign.js
@@ -1,40 +1,57 @@
 (function()
 {
-    "use strict"
+  "use strict";
+  var isEnumerable = {}.propertyIsEnumerable
 
-    if (typeof Object.deepAssign == "function") return
+  if (typeof Object.deepAssign == "function") return
 
-    var isEnumerable = {}.propertyIsEnumerable
+  function isObject(value) {
+      return value != null && typeof value === 'object';
+    }
 
-    function isObject(value){
-      return value != null && typeof value === 'object'
-      }
+  Object.defineProperty(Object, "deepAssign",
+  {
+    value: function deepAssign(target, sources)
+    {
+      if (target == null) throw new TypeError('Target object cannot be null');
 
-    Object.defineProperty(Object, "deepAssign",{
-        value: function deepAssign(target, sources){
-            if (target == null) throw new TypeError
+      var to = Object(target);
 
-            var to = Object(target)
+      for (var index = 1; index < arguments.length;)
+      {
+        var from = arguments[index++];
+        if (isObject(from)){
+          Reflect.ownKeys(from).forEach(function(key)
+          {
+            if (isEnumerable.call(from, key)) {
 
-            for (var index = 1; index < arguments.length ;){
-              var from = arguments[index++]
+              if(isObject(to[key]) && isObject(from[key])) {
 
-              if(from === isObject(from)){
-                Object.keys(Object(from)).forEach(function(key){
-                  if (isEnumerable.call(from, key))
-                  to[key] = deepAssign(from[key], from[key])
-                })
+                to[key] = Object.deepAssign(to[key], from[key]);
+
+              } 
+              else if(isObject(to[key]) && from[key] instanceof Date ||
+                                           from[key] instanceof RegExp ||
+                                           from[key] instanceof Map ||
+                                           from[key] instanceof Set){
+                to[key] = new from[key].constructor(from[key]);
               }
-              else{
-                Object.keys(Object(from)).forEach(function(key){
-                    if (isEnumerable.call(from, key))
-                        to[key] = from[key]
-                })
+              else {
+              to[key] = from[key];
               }
             }
-          return to
-        },
-        writable: true,
-        configurable: true
-    })
+          })
+        }
+        else{
+          console.log("Not an object");
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  })
 })()
+
+Object.deepAssign({a: {b: 0}}, {a: {b: 1, c: 2}}, {a: {c: 3}});
+                          /*     {a: {b: 1, c: 3}}*/
